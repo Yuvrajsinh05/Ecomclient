99b2afadbf4ab75c81a54052b1b4bf179e8c296a
Yuvrajsinh05 <yuvrajsinh.jadav.21M@iitram.ac.in>
2024-04-01 15:36:22 +0530

    fixed :  -

--- src/routes/home/products/Products.js
+++ src/routes/home/products/Products.js
@@ -73,34 +73,45 @@ function FeaturedCard({fpro}) {
       };
     return (
         <>
-              <div style={{height:'320px'}} className={styles.product_container} >
-                <div className="product-item bg-light mb-4">
-                    <div className="product-img position-relative overflow-hidden">
-                        <img style={{height:"200px"}} className="img-fluid w-100" src={fpro?.image} alt="" />
-                        <div className="product-action">
-                            <Link className="btn btn-outline-dark btn-square" to={`/shopdetail/${fpro?._id}`}><i className="fa fa-shopping-cart"></i></Link>
-                            <Link className="btn btn-outline-dark btn-square"  onClick={handleLikeProduct}><i className="far fa-heart"></i></Link>
-                            {/* <Link className="btn btn-outline-dark btn-square" href=""><i className="fa fa-sync-alt"></i></Link> */}
-                            {/* <Link className="btn btn-outline-dark btn-square" href=""><i className="fa fa-search"></i></Link> */}
-                        </div>
-                    </div>
-                    <div className="text-center py-4">
-                        <Link className="h6 text-decoration-none text-truncate" href="">{fpro?.model}</Link>
-                        <div className="d-flex align-items-center justify-content-center mt-2">
-                            <h5>{"₹"+ fpro?.price}</h5><h6 className="text-muted ml-2"><del>$123.00</del></h6>
-                        </div>
-                        <div className="d-flex align-items-center justify-content-center mb-1">
-                            <small className="fa fa-star text-primary mr-1"></small>
-                            <small className="fa fa-star text-primary mr-1"></small>
-                            <small className="fa fa-star text-primary mr-1"></small>
-                            <small className="fa fa-star text-primary mr-1"></small>
-                            <small className="fa fa-star text-primary mr-1"></small>
-                            <small>(99)</small>
-                        </div>
-                    </div>
-                </div>
+           <div style={{height:'320px'}} className={styles.product_container} >
+        <div  className="product-item bg-light mb-4">
+          <div className="product-img position-relative overflow-hidden">
+            <img
+              className="img-fluid w-100" style={{ height: "200px" }}
+              src={fpro?.image || fpro?.imageUrl}
+              alt=""
+            />
+            <div className="product-action">
+              <Link className="btn btn-outline-dark btn-square" to={`/shopdetail/${fpro?._id}`}>
+                <i className="fa fa-shopping-cart"></i>
+              </Link>
+              <Link onClick={handleLikeProduct} className="btn btn-outline-dark btn-square" to="">
+                <i className="far fa-heart fa-solid"></i>
+              </Link>
+  
             </div>
-            
+          </div>
+          <div className="text-center py-4">
+            <Link className="h6 text-decoration-none text-truncate" style={{display:'block'}} to="">
+              {fpro?.name || fpro?.model}
+            </Link>
+            <div className="d-flex align-items-center justify-content-center mt-2">
+              <h5>{`₹${fpro.price}`}</h5>
+              <h6 className="text-muted ml-2">
+                <del>{`₹${fpro.price}`}</del>
+              </h6>
+            </div>
+            <div className="d-flex align-items-center justify-content-center mb-1">
+              <small className="fa fa-star text-primary mr-1"></small>
+              <small className="fa fa-star text-primary mr-1"></small>
+              <small className="fa fa-star text-primary mr-1"></small>
+              <small className="fa fa-star text-primary mr-1"></small>
+              <small className="fa fa-star text-primary mr-1"></small>
+              <small>(99)</small>
+            </div>
+          </div>
+        </div>
+      </div>
             </>
     )
 }
